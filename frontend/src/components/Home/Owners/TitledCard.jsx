b85faf153cdbb118f31ca79b0305ef605db48654
Tuhin <dastuhin.rnj@gmail.com>
2025-01-29 02:35:36 +0530

    Owners

--- frontend/src/components/Home/Owners/TitledCard.jsx
+++ frontend/src/components/Home/Owners/TitledCard.jsx
@@ -3,7 +3,7 @@ import './TitledCard.css'
 
 function TitledCard({ source, alt, name }) {
     return (
-        <div className='max-w-[400px] card bg-black'>
+        <div className='md:max-w-[300px] card bg-black'>
             <div className='bg-black p-4 rounded-[15px]'>
                 <img src={source} alt={alt} className='w-full h-full object-cover object-top rounded-[10px] aspect-square' />
                 <h3 className='mt-2'>{name}</h3>
